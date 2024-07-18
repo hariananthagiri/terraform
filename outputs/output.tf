ec9129df834f4473877305c589ef12b36cfe2830
hariananthagiri <hariananthagiri@gmail.com>
2024-07-18 14:26:27 +0530

    initial commit

--- outputs/output.tf
+++ outputs/output.tf
@@ -1,3 +1,5 @@
 output "instance_private_ip" {
 value = aws_instance.web.private_ip  
-}
+# description = "optional"  # it describes about the information about output
+# sensitive = true/false  # it makes the information visable or disable if content is sensitive
+ }
