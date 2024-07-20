8d2eff9834729518560fdff299aefbde69deecba
hariananthagiri <hariananthagiri@gmail.com>
2024-07-20 20:45:43 +0530

    initial commit

--- resources/vpc/routes.tf
+++ resources/vpc/routes.tf
@@ -1,14 +1,17 @@
-# public route creation
+# # public route creation
 
-resource "aws_route" "public_route" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
+# resource "aws_route" "public_route" {
+#   route_table_id            = aws_route_table.public.id
+#   vpc_peering_connection_id = "give id"
+#   destination_cidr_block    = "0.0.0.0/0"
+#   gateway_id = aws_internet_gateway.igw.id
+# }
 
-# private route creation
+# # private route creation
 
-resource "aws_route" "private_route" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-}
+# resource "aws_route" "private_route" {
+#   route_table_id            = aws_route_table.private.id
+#   vpc_peering_connection_id = "give id"
+#   destination_cidr_block    = "0.0.0.0/0"
+# }
+# we use it for vpc_peering
