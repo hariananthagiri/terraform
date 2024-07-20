607320b4809292a4ce5bea0e81cf8fb353be2215
hariananthagiri <hariananthagiri@gmail.com>
2024-07-20 21:01:53 +0530

    initial commit

--- resources/vpc/routes.tf
+++ resources/vpc/routes.tf
@@ -1,15 +1,15 @@
-# public route creation
+# # public route creation
 
-resource "aws_route" "public_route" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
+# resource "aws_route" "public_route" {
+#   route_table_id            = aws_route_table.public.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   gateway_id = aws_internet_gateway.igw.id
+# }
 
-# private route creation
+# # private route creation
 
-resource "aws_route" "private_route" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  # nat_gateway_id = aws_nat_gateway.main.id
-}
+# resource "aws_route" "private_route" {
+#   route_table_id            = aws_route_table.private.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   # nat_gateway_id = aws_nat_gateway.main.id
+# }
