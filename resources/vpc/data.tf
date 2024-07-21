85c2a83a859a09d36d14d93789e4f1818eab5555
hariananthagiri <hariananthagiri@gmail.com>
2024-07-21 07:53:58 +0530

    initial commit

--- resources/vpc/data.tf
+++ resources/vpc/data.tf
@@ -1,3 +1,11 @@
 data "aws_vpc" "default" {
    default = true
  }
+
+ data "aws_route_table" "default" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+        name = "association.main"
+        values = ["true"]
+    }
+}
