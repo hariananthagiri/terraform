4e1366849ee18a79fdafecf3e0a39ba5779ee8f6
hariananthagiri <hariananthagiri@gmail.com>
2024-07-22 22:19:45 +0530

    initial commit

---
+++ loops/01-count/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web" {     # terraform reference name
+  count = 2
+  ami           = "ami-0b4f379183e5706b9" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform" # instance name
+  }
+}
