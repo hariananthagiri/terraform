fd05187f8f2b65317e324a61ce7095f573d86373
hariananthagiri <hariananthagiri@gmail.com>
2024-07-23 07:01:30 +0530

    initial commit

--- functions/02-map/providers.tf
+++ functions/02-map/providers.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.58.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+  }
+}
 
-# provider "aws" {
-#   # Configuration options
-# }
+provider "aws" {
+  # Configuration options
+}
