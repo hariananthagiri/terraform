57a54a71006eebf59a036a37a4a38fdd09bbbf8e
hariananthagiri <hariananthagiri@gmail.com>
2024-07-23 06:50:55 +0530

    initial commit

--- from_github/module.tf
+++ from_github/module.tf
@@ -1,6 +1,9 @@
 module "ec2_instance" {
     #source = "git::https://github.com/shivaananthagiri/Terraform.git?ref=main"   # give the path here
     # variable = "value"
-    source = "git::https://github.com/hariananthagiri/terraform.git//instances?ref=main"   # give the path here
+    # # refering sub-folder
+    # source = "git::https://github.com/hariananthagiri/terraform.git//instances?ref=main"   # give the path here
+     source = "git::https://github.com/hariananthagiri/terraform.git//functions/02-map?ref=main"   # give the path here
+
 
 }
