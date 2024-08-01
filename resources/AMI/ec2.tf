3d5400b19f91a009815195d9355ccfbe2c5c1d83
hariananthagiri <hariananthagiri@gmail.com>
2024-08-01 18:49:02 +0530

    initial commit

--- resources/AMI/ec2.tf
+++ resources/AMI/ec2.tf
@@ -3,6 +3,7 @@
 resource "aws_instance" "web" {     # terraform reference name
   ami           = var.ami_id 
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
   tags = var.tags
 }
 
---
+++ resources/AMI/sg.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "roboshop-all" { 
+    name        = "terraform_sg for our reference"   # aws security group name  ixed
+    description = "createing a security group from terraform"
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = 22 
+        to_port          = 22 
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = 80 
+        to_port          = 80 
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "terraform_sg"  # aws security group name it can be editable
+    }
+}
