1ffc8a879fef99bbef4ed0e4583420517dad0c06
hariananthagiri <hariananthagiri@gmail.com>
2024-08-01 12:40:17 +0530

    initial commit

--- resources/ec2/ec2.tf
+++ resources/ec2/ec2.tf
@@ -1,5 +1,25 @@
+# to create a ec2 instance
+
 resource "aws_instance" "web" {     # terraform reference name
   ami           = var.ami_id 
   instance_type = var.instance_type
   tags = var.tags
 }
+
+# # to stop a ec2 instance
+
+# resource "aws_ec2_instance_state" "web" {     # terraform reference name
+#   instance_id = aws_instance.web.id
+#   state = "stopped"
+# }
+
+
+# # to start a stoped ec2 instance
+
+# resource "aws_ec2_instance_state" "web" {     # terraform reference name
+#   instance_id = aws_instance.web.id
+#   state = "running"
+# }
+
+# # to terminate a ec2 instance
+#  terraform destroy -auto-approve -target aws_instance.web
