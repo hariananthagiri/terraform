f12f80c250ae7cb4a0f8fa7e57d3671240a551fa
hariananthagiri <hariananthagiri@gmail.com>
2024-08-07 08:34:16 +0530

    initial commit

--- variables/01-list/output.tf
+++ variables/01-list/output.tf
@@ -7,9 +7,3 @@
 # output "instance_types" {
 #   value = element(var.instance_type, length(var.instance_type)-1)
 # }
-
-# # reading elements in a list in reverse order
-# output "instance_types" {
-#   value = reverse(var.instance_type[*])
-# }
-
